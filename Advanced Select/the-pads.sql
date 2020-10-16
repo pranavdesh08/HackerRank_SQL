5fb6965de4160fb80d0e1ffedd61bfc6e10eb5ce
Pranav Deshpande <53112948+pranavdesh08@users.noreply.github.com>
2020-10-16 17:33:04 -0400

    Create the-pads.sql

---
+++ Advanced Select/the-pads.sql
@@ -0,0 +1,3 @@
+select concat(Name,'(', substring(Occupation,1,1),')') from OCCUPATIONS order by Name;
+
+select concat('There are a total of ', count(Occupation),' ', lower(Occupation),'s.') from OCCUPATIONS group by Occupation order by count(Occupation), Occupation
